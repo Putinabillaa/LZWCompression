51dcf3de7651f40f629e3ea5bcd02c04eb6cf550
Putinabillaa <aidiraputiabil@gmail.com>
2023-06-28 11:40:38 +0800

    minor fix UI, BE settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-zulj-$c%9roy^idc=4z(ao@#58(8wh!2hx%rst4amucolzn@zq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
